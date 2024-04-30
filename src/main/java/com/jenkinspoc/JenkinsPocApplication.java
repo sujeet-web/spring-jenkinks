4bcfb07a9612a6bb3dc33158309fb8ad9dc96b89
sujeet-web <sujeetsharma675@gmail.com>
2024-04-30 19:47:38 +0530

    first commit

---
+++ src/main/java/com/jenkinspoc/JenkinsPocApplication.java
@@ -0,0 +1,25 @@
+package com.jenkinspoc;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPocApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(JenkinsPocApplication.class);
+
+	@PostConstruct
+	public void intt() {
+		logger.info("Application started.....");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed.....");
+		SpringApplication.run(JenkinsPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkinspoc/JenkinsPocApplicationTests.java
@@ -0,0 +1,21 @@
+package com.jenkinspoc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPocApplicationTests {
+	
+	public static Logger logger = LoggerFactory.getLogger(JenkinsPocApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case executing......");
+		assertEquals(true, true);
+	}
+
+}
